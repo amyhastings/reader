975ade8536f3fc35f2792ef478e4d0a58503790a
Amy Hastings <amy.hastings@gmail.com>
2025-03-06 16:33:55 +0000

    Got basic functionality for search working - formatting etc to do

--- reader/search/views.py
+++ reader/search/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from openlibrary.openlibrary import *
 
 def search(request):
-    return render(request, 'search/search.html')
+    searchString = request.GET.get('q', False)
+    results = '{}'
+    if searchString:
+        client = OpenLibraryClient()
+        author_results = client.search_author(searchString)
+        book_results = client.search_book(searchString)
+    return render(request, 'search/search.html', {'author_results': author_results, 'book_results': book_results})
