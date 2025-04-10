78445cce38768f8dee0fcbef5f5ccb2d01a35075
Amy Hastings <amy.hastings@gmail.com>
2025-04-10 15:20:51 +0100

    Amendment to error message html

--- reader/reader/views.py
+++ reader/reader/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, get_object_or_404
 
 
 def error_500(request):
-    return render(request, 'reader/500.html', status=500)
+    return render(request, 'reader/error_500.html', status=500)
